06e295b37f7a07f392227adc7c24aa1deb24bfb1
Marlon Braga <rdg.brg@gmail.com>
2020-04-29 15:17:03 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -11,25 +11,3 @@ def find_max_value(array)
   end
   return maxVal
 end
-
-
-find_max_value([5,4,3,6,2,1])
-
-
-# [5,4,3,2,1]
-# maxVal = 5
-
-# 5 < 4 NO then loop
-# 5 < 3 NO then loop
-# 5 < 2 NO then loop
-# 5 < 1 NO then loop - END LOOP AND RETURN maxVal-
-
-# [5,4,3,6,1,2]
-
-# maxVal = 5
-# 5 < 4 NO, then loop
-# 5 < 3 NO, then loop
-# 5 < 6 YES, reassign maxVal to 6
-# maxVal = 6
-# 6 < 1 NO, then loop
-# 6 < 2 NO, then loop - END LOOP AND RETURN maxVal -
